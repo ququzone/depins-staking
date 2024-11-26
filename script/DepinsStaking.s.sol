2a46fde110ee5afce9ea2793ab9c89f2aa142e32
ququzone <xueping.yang@gmail.com>
2024-11-26 21:53:14 +0800

    chore: set forge

---
+++ script/DepinsStaking.s.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Script, console} from "forge-std/Script.sol";
+
+contract DepinsStakingScript is Script {
+
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+        vm.stopBroadcast();
+    }
+}
---
+++ test/DepinsStaking.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Test, console} from "forge-std/Test.sol";
+import {DepinsStaking} from "../src/DepinsStaking.sol";
+
+contract DepinsStakingTest is Test {
+    DepinsStaking public staking;
+
+    function setUp() public {
+        staking = new DepinsStaking();
+    }
+}
