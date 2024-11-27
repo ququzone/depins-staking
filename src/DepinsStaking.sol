f6346e026656c9c27d93e48f62d78ad52d56d517
ququzone <xueping.yang@gmail.com>
2024-11-27 10:18:53 +0800

    feat: complete stake/unstake/withdraw

--- src/DepinsStaking.sol
+++ src/DepinsStaking.sol
@@ -11,6 +11,7 @@ contract DepinsStaking is OwnableUpgradeable, ERC721EnumerableUpgradeable {
 
     struct StakingType {
         uint64 stakingPeriod;
+        uint64 freezenPeriod;
         uint64 stakingRate;
     }
 
@@ -22,11 +23,25 @@ contract DepinsStaking is OwnableUpgradeable, ERC721EnumerableUpgradeable {
         uint256 stakingAmount;
     }
 
-    event NewStakingType(uint64 stakingType, uint64 stakingPeriod, uint64 stakingRate);
+    event StopStake();
+    event StartStake();
+    event NewStakingType(uint64 stakingType, uint64 stakingPeriod, uint64 freezonPeriod, uint64 stakingRate);
     event RemoveStakingType(uint64 stakingType);
     event EmergencyWithdraw(uint256 tokenId, uint256 amount);
+    event Stake(
+        address indexed staker,
+        uint256 tokenId,
+        uint64 stakingType,
+        uint64 stakingRate,
+        uint64 withdrawTime,
+        uint256 stakingAmount
+    );
+    event Unstake(address indexed operator, uint256 tokenId);
+    event Withdraw(address indexed owner, uint256 tokenId, uint256 amount, uint256 yield);
 
     IERC20 public immutable depins;
+    uint256 nextId;
+    bool public stakeable;
     mapping(uint64 => StakingType) public stakingType;
     mapping(uint256 => Bag) public bags;
 
@@ -39,14 +54,36 @@ contract DepinsStaking is OwnableUpgradeable, ERC721EnumerableUpgradeable {
         __Ownable_init(msg.sender);
         __ERC721_init(_name, _symbol);
         __ERC721Enumerable_init();
+        stakeable = true;
     }
 
-    function newStakingType(uint64 _stakingType, uint64 _stakingPeriod, uint64 _stakingRate) external onlyOwner {
-        require(_stakingPeriod > 0 && _stakingRate > 0 && _stakingRate < 1000000, "invalid params");
+    function stopStake() external onlyOwner {
+        require(stakeable, "stopped");
+
+        stakeable = false;
+        emit StopStake();
+    }
+
+    function startStake() external onlyOwner {
+        require(!stakeable, "started");
+
+        stakeable = true;
+        emit StartStake();
+    }
+
+    function newStakingType(uint64 _stakingType, uint64 _stakingPeriod, uint64 _freezenPeriod, uint64 _stakingRate)
+        external
+        onlyOwner
+    {
+        require(
+            _stakingPeriod > 0 && _stakingRate > 0 && _stakingRate < 1000000 && _stakingPeriod % 86400 == 0,
+            "invalid params"
+        );
         require(stakingType[_stakingType].stakingPeriod == 0, "type exists");
 
-        stakingType[_stakingType] = StakingType({stakingPeriod: _stakingPeriod, stakingRate: _stakingRate});
-        emit NewStakingType(_stakingType, _stakingPeriod, _stakingRate);
+        stakingType[_stakingType] =
+            StakingType({stakingPeriod: _stakingPeriod, freezenPeriod: _freezenPeriod, stakingRate: _stakingRate});
+        emit NewStakingType(_stakingType, _stakingPeriod, _freezenPeriod, _stakingRate);
     }
 
     function removeStakingType(uint64 _stakingType) external onlyOwner {
@@ -56,21 +93,71 @@ contract DepinsStaking is OwnableUpgradeable, ERC721EnumerableUpgradeable {
         emit RemoveStakingType(_stakingType);
     }
 
-    function stake() external returns (uint256) {}
+    function stake(uint64 _stakingType, uint256 _stakingAmount) external returns (uint256 tokenId_) {
+        require(_stakingAmount > 0, "zero amount");
+        require(stakeable, "stake stopped");
 
-    function unstake(uint256 _tokenId) external returns (bool) {}
+        StakingType memory _type = stakingType[_stakingType];
+        require(_type.stakingPeriod > 0, "type not exists");
 
-    function withdraw(uint256 _tokenId) external returns (uint256) {}
+        depins.safeTransferFrom(msg.sender, address(this), _stakingAmount);
+        tokenId_ = nextId;
+        ++nextId;
 
-    function emergencyWithdraw(uint256 _tokenId) external returns (uint256) {
-        require(ownerOf(_tokenId) == msg.sender, "invalid owner");
+        uint64 _now = uint64(block.timestamp) / 86400 * 86400;
+        uint64 _withdrawTime = 0;
+        if (_type.freezenPeriod > 0) {
+            _withdrawTime = _now + _type.stakingPeriod;
+        }
 
-        uint256 _amount = bags[_tokenId].stakingAmount;
+        bags[tokenId_] = Bag({
+            stakingType: _stakingType,
+            stakingTime: _now,
+            stakingRate: _type.stakingRate,
+            withdrawTime: _withdrawTime,
+            stakingAmount: _stakingAmount
+        });
+        _mint(msg.sender, tokenId_);
+
+        emit Stake(msg.sender, tokenId_, _stakingType, _type.stakingRate, _withdrawTime, _stakingAmount);
+    }
+
+    function unstake(uint256 _tokenId) external returns (bool success_) {
+        address _owner = _requireOwned(_tokenId);
+        if (stakeable) {
+            require(_owner == msg.sender, "only owner");
+        }
+
+        Bag storage _bag = bags[_tokenId];
+        require(_bag.withdrawTime == 0, "unstaked");
+
+        _bag.withdrawTime = uint64(block.timestamp) / 86400 * 86400;
+        success_ = true;
+
+        emit Unstake(msg.sender, _tokenId);
+    }
+
+    function withdraw(uint256 _tokenId) external returns (uint256 amount_) {
+        address _owner = _requireOwned(_tokenId);
+        Bag memory _bag = bags[_tokenId];
+        require(_bag.withdrawTime > 0, "staking");
+
+        uint256 _amount = _bag.stakingAmount;
+        uint256 yield = ((_bag.withdrawTime - _bag.stakingTime) / 86400 * _bag.stakingRate) * _amount / 1000000;
         delete bags[_tokenId];
         _burn(_tokenId);
-        depins.safeTransfer(msg.sender, _amount);
-        emit EmergencyWithdraw(_tokenId, _amount);
-        return _amount;
+        depins.safeTransfer(_owner, amount_ + yield);
+        emit Withdraw(_owner, _tokenId, _amount, yield);
+    }
+
+    function emergencyWithdraw(uint256 _tokenId) external returns (uint256 amount_) {
+        require(ownerOf(_tokenId) == msg.sender, "invalid owner");
+
+        amount_ = bags[_tokenId].stakingAmount;
+        delete bags[_tokenId];
+        _burn(_tokenId);
+        depins.safeTransfer(msg.sender, amount_);
+        emit EmergencyWithdraw(_tokenId, amount_);
     }
 
     function adminWithdraw() external onlyOwner {
